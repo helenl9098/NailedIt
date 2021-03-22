c10b0a0695605c3525a795b34917d5d5bf907269
Helen <liuhanyu@sas.upenn.edu>
2021-03-22 02:02:18 -0400

    first commit

---
+++ Assets/Mirror/Runtime/NetworkManagerHUD.cs
@@ -0,0 +1,164 @@
+// vis2k: GUILayout instead of spacey += ...; removed Update hotkeys to avoid
+// confusion if someone accidentally presses one.
+
+using System;
+using UnityEngine;
+
+namespace Mirror
+{
+    /// <summary>
+    /// An extension for the NetworkManager that displays a default HUD for controlling the network state of the game.
+    /// <para>This component also shows useful internal state for the networking system in the inspector window of the editor. It allows users to view connections, networked objects, message handlers, and packet statistics. This information can be helpful when debugging networked games.</para>
+    /// </summary>
+    [DisallowMultipleComponent]
+    [AddComponentMenu("Network/NetworkManagerHUD")]
+    [RequireComponent(typeof(NetworkManager))]
+    [HelpURL("https://mirror-networking.com/docs/Articles/Components/NetworkManagerHUD.html")]
+    public class NetworkManagerHUD : MonoBehaviour
+    {
+        NetworkManager manager;
+
+        /// <summary>
+        /// Whether to show the default control HUD at runtime.
+        /// </summary>
+        [Obsolete("showGUI will be removed unless someone has a valid use case. Simply use or don't use the HUD component.")]
+        public bool showGUI = true;
+
+        /// <summary>
+        /// The horizontal offset in pixels to draw the HUD runtime GUI at.
+        /// </summary>
+        public int offsetX;
+
+        /// <summary>
+        /// The vertical offset in pixels to draw the HUD runtime GUI at.
+        /// </summary>
+        public int offsetY;
+
+        void Awake()
+        {
+            manager = GetComponent<NetworkManager>();
+        }
+
+        void OnGUI()
+        {
+#pragma warning disable 618
+            if (!showGUI) return;
+#pragma warning restore 618
+            GUI.skin.label.fontSize = 50;
+            GUI.skin.button.fontSize = 50;
+            GUI.skin.textField.fontSize = 50;
+            GUILayout.BeginArea(new Rect(10 + offsetX, 40 + offsetY, 700, 2000));
+            if (!NetworkClient.isConnected && !NetworkServer.active)
+            {
+                StartButtons();
+            }
+            else
+            {
+                StatusLabels();
+            }
+
+            // client ready
+            if (NetworkClient.isConnected && !ClientScene.ready)
+            {
+                if (GUILayout.Button("Client Ready", GUILayout.Height(100)))
+                {
+                    ClientScene.Ready(NetworkClient.connection);
+
+                    if (ClientScene.localPlayer == null)
+                    {
+                        ClientScene.AddPlayer(NetworkClient.connection);
+                    }
+                }
+            }
+
+            StopButtons();
+
+            GUILayout.EndArea();
+        }
+
+        void StartButtons()
+        {
+            if (!NetworkClient.active)
+            {
+                // Server + Client
+                if (Application.platform != RuntimePlatform.WebGLPlayer)
+                {
+                    if (GUILayout.Button("Host (Server + Client)", GUILayout.Height(100)))
+                    {
+                        manager.StartHost();
+                    }
+                }
+
+                // Client + IP
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("Client", GUILayout.Height(100)))
+                {
+                    manager.StartClient();
+                }
+                manager.networkAddress = GUILayout.TextField(manager.networkAddress, GUILayout.Height(100));
+                GUILayout.EndHorizontal();
+
+                // Server Only
+                if (Application.platform == RuntimePlatform.WebGLPlayer)
+                {
+                    // cant be a server in webgl build
+                    GUILayout.Box("(  WebGL cannot be server  )", GUILayout.Height(100));
+                }
+                else
+                {
+                    if (GUILayout.Button("Server Only", GUILayout.Height(100))) manager.StartServer();
+                }
+            }
+            else
+            {
+                // Connecting
+                GUILayout.Label("Connecting to " + manager.networkAddress + "..", GUILayout.Height(100));
+                if (GUILayout.Button("Cancel Connection Attempt", GUILayout.Height(100)))
+                {
+                    manager.StopClient();
+                }
+            }
+        }
+
+        void StatusLabels()
+        {
+            // server / client status message
+            if (NetworkServer.active)
+            {
+                GUILayout.Label("Server: active. Transport: " + Transport.activeTransport, GUILayout.Height(100));
+            }
+            if (NetworkClient.isConnected)
+            {
+                GUILayout.Label("Client: address=" + manager.networkAddress, GUILayout.Height(100));
+            }
+        }
+
+        void StopButtons()
+        {
+            // stop host if host mode
+            if (NetworkServer.active && NetworkClient.isConnected)
+            {
+                if (GUILayout.Button("Stop Host", GUILayout.Height(100)))
+                {
+                    manager.StopHost();
+                }
+            }
+            // stop client if client-only
+            else if (NetworkClient.isConnected)
+            {
+                if (GUILayout.Button("Stop Client", GUILayout.Height(100)))
+                {
+                    manager.StopClient();
+                }
+            }
+            // stop server if server-only
+            else if (NetworkServer.active)
+            {
+                if (GUILayout.Button("Stop Server", GUILayout.Height(100)))
+                {
+                    manager.StopServer();
+                }
+            }
+        }
+    }
+}
